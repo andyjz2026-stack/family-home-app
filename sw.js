06a0742d411ab98537fa0397b41379383c15c148
andyjz2026-stack <andyjz2026-stack@users.noreply.github.com>
2026-09-23 09:28:31 +0000

    deploy: 5500913c87d77f119f019ed8841c7422dacda657

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).t
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+  event.respondWith(fetch(event.request).then((response) => {
     const copy = response.clone();
     caches.open(CACHE).then((cache) => cache.put(event.request, copy));
     return response;
-  }).catch(() => caches.match(`${BASE}index.html`))));
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match(`${BASE}index.html`))));
 });
